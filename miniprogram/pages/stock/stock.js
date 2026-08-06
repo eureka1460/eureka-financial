022826280e7696dea3a493e2770400fb54d38947
eureka1460 <2259932643@qq.com>
2026-08-07 00:23:15 +0800

    圆点叠柱子上+气泡加宽同行

--- miniprogram/pages/stock/stock.js
+++ miniprogram/pages/stock/stock.js
@@ -203,7 +203,7 @@ Page({
     this.setData({
       tooltip: {
         idx, x,
-        txt: bar._lbl + '\n  ' + bar._val + '\n' + (bar._yTxt || '同比 --'),
+        txt: bar._lbl + '  |  ' + bar._val + '  |  ' + (bar._yTxt || '同比 --'),
       },
     });
   },
